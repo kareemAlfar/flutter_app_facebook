b4d4eaa5db38c027fd9cc48508ce861f043be78b
unknown <kareemelfara@gmail.com>
2022-03-11 18:22:58 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,15 @@
+
+
+// echo "# js" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/kareemAlfar/flutter_app_facebook
+// git push -u origin main
+
+
+
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
